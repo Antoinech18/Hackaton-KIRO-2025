c0e1d678246f2d49855025fd623dfdf33e98e9c6
Antoinech18 <antoinech18@hotmail.fr>
2025-11-13 13:52:11 +0100

    commentaires

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("sujet/instances/vehicles.csv")
+##### Extraction des données #####
 
-columns = data.columns[1:]
+data_vehicles = pd.read_csv("sujet/instances/vehicles.csv")
+
+columns = data_vehicles.columns[1:]
 # ['max_capacity', 'rental_cost', 'fuel_cost', 'radius_cost', 'speed',
 #       'parking_time', 'fourier_cos_0', 'fourier_sin_0', 'fourier_cos_1',
 #       'fourier_sin_1', 'fourier_cos_2', 'fourier_sin_2', 'fourier_cos_3',
@@ -14,8 +16,8 @@ vehicles2 = {}
 vehicles3 = {}
 
 for c in columns: 
-    vehicles1[c] = data.loc[0][c]
-    vehicles2[c] = data.loc[1][c]
-    vehicles3[c] = data.loc[2][c]
+    vehicles1[c] = data_vehicles.loc[0][c]
+    vehicles2[c] = data_vehicles.loc[1][c]
+    vehicles3[c] = data_vehicles.loc[2][c]
 
-data_vehicles = [vehicles1, vehicles2, vehicles3]
+vehicles = [vehicles1, vehicles2, vehicles3]
